5d9755deb3f0d1e1b552464979db362588e2a50b
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-08-10 16:16:27 -0700

    state.py

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a state
+
+    Attributes:
+        name: string - empty string
+
+    """
+
+    name = ""
+
