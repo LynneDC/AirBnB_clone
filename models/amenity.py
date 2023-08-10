1f5d93c59f000e4565d4c7d2a5258b496b47220a
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-08-10 16:15:36 -0700

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Custom amenity class
+
+    Attributes:
+        name: string - empty string
+
+    """
+    name = ""
