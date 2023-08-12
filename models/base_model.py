211fbe8b1d0b57c29c3cbe71f8e98f7515164b0d
LynneDC <roselinedc1308@gmail.com>
2023-08-12 02:19:45 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -40,15 +40,12 @@ class BaseModel:
                             value = datetime.strptime(
                                 value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
-
-
         else:
 
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-
 
     """public methods to modify public instances"""
     def save(self):
@@ -69,8 +66,6 @@ class BaseModel:
         if isinstance(obj_dict["updated_at"], datetime):
             obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
         return obj_dict
-
-
 
     def __str__(self):
         return "[{}] ({}) {}".format(
