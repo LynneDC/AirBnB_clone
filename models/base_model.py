50334de623445c318772b61c1b6ad49ab4c145b2
LynneDC <roselinedc1308@gmail.com>
2023-08-12 15:58:28 +0200

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,73 +1,71 @@
 #!/usr/bin/python3
-"""import class uuid for unique id
-and datetime for datetimeof uuid creation
 """
-
+Import the `uuid` class for generating unique IDs
+Import the `datetime` class for representing the datetime of UUID creation
+"""
 import uuid
 from models import storage
 from datetime import datetime
 
-
 """
-define a class basemodel that is
-the parrent class to the all attribuets and
-methods
-puplic  instance attributes:
-    id: is assigned with uuid on creation
-    created_at: curret datime of id creation
-    updated_at: curreect time of update id
-output format: [class_name] (id) dict
-public instance methods:
-    save(): updates updated_at with current date
-    to_dict(): returns dict uses:
-        self.__dict__: return only inst attr
-        __class__:added to dict with cls_name
-        created_at & updated_at: converted to isoformat()
-        [%Y-%m-%dT%H:%M:%S.%f for2017-06-14T22:31:03.285259
-Return:
-    object_dict
+Def class BaseModel parent class for all attributes and methods.
+Public instance attributes:
+    - `id`: assigned with a UUID on creation
+    - `created_at`: current datetime of ID creation
+    - `updated_at`: current time of ID update
+Output format: [class_name] (id) dict
+Public instance methods:
+    - `save()`: updates `updated_at` with the current date
+    - `to_dict()`: returns a dictionary representation of the instance:
+        - `self.__dict__`: returns only the instance attributes
+        - `__class__`: added to the dictionary with the class name
+        - `created_at` and `updated_at`: converted to ISO format
+    - `object_dict`: dictionary representation of the instance
 """
 
 
 class BaseModel:
-    """ data declaration and initilization"""
+    """Class initialization and data declaration"""
     def __init__(self, *args, **kwargs):
+        # If keyword arguments are provided
         if kwargs:
             for key, value in kwargs.items():
+                # Exclude the "__class__" attribute
                 if key != "__class__":
                     if isinstance(value, str):
+                        # Convert "created_at" and "updated_at"
+                        # strings to datetime objects
                         if key in ["created_at", "updated_at"]:
                             value = datetime.strptime(
                                 value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
-
+            # Generate a new UUID for the "id" attribute
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
 
-    """public methods to modify public instances"""
+    """Public methods to modify public instance attributes"""
     def save(self):
         self.updated_at = datetime.now()
-
         storage.save()
         return "[{}] ({}) {}".format(
-                        self.__class__.__name__,
-                        self.id, self.__dict__)
+            self.__class__.__name__, self.id, self.__dict__)
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
 
         if isinstance(obj_dict["created_at"], datetime):
+            # Convert "created_at" to ISO format
             obj_dict["created_at"] = obj_dict["created_at"].isoformat()
 
         if isinstance(obj_dict["updated_at"], datetime):
+            # Convert "updated_at" to ISO format
             obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
         return obj_dict
 
     def __str__(self):
         return "[{}] ({}) {}".format(
-                        self.__class__.__name__,
-                        self.id, self.__dict__)
+            self.__class__.__name__, self.id, self.__dict__)
