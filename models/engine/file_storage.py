4a33cacf41493fb168b08cd2625c1bc6d29a6aba
LynneDC <roselinedc1308@gmail.com>
2023-08-12 02:16:05 +0200

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-""" importing json module and"""
+# importing json module and
 import json
-#import models
+
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
+
     def all(self):
         return FileStorage.__objects
+
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
+
     def save(self):
         data = {}
         for key, value in FileStorage.__objects.items():
             data[key] = value.to_dict()
         with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
             json.dump(data, file)
+
     def reload(self):
         from models.base_model import BaseModel
         data_classes = {"BaseModel": BaseModel}
@@ -25,7 +29,7 @@ class FileStorage:
                 data = json.load(file)
                 for values in data.values():
                     name = values["__class__"]
-                    
+
                     obj = data_classes[name]
                     self.new(obj(**values))
         except FileNotFoundError:
