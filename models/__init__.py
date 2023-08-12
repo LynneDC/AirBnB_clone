e57c2e84e6bc76e57906c813015a5b44cadb1ba3
LynneDC <roselinedc1308@gmail.com>
2023-08-12 02:10:27 +0200

    linker

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,6 @@
 """ import storagemodule from models that is and link BaseModel with filestorage
 """
 from models.engine.file_storage import FileStorage
-#from models.base_model import BaseModel
 
 storage = FileStorage()
 storage.reload()
