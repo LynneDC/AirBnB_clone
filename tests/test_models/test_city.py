32e57331a59c1ee855dea522c3c8568b46a352c0
LynneDC <roselinedc1308@gmail.com>
2023-08-12 17:22:13 +0200

    tests

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+import datetime
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+
+if __name__ == "__main__":
+    # Run the tests
+    unittest.main()
